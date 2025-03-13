eb15c4f80a5d4d1c4646d4015a0dc3748f4868ad
Yang <hoanglap644@gmail.com>
2025-03-13 20:28:48 +0700

    Push

---
+++ Assets/Scripts/PlayerSpawmer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Fusion;
+
+public class PlayerSpawmer: SimulationBehaviour, IPlayerJoined
+{
+    public GameObject PlayerPrefab;
+    
+        public void PlayerJoined(PlayerRef player)
+        {
+            if (player == Runner.LocalPlayer)
+            {
+                Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+            }
+        }
+}
